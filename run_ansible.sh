6e37a55b94b89fe3a616493e61f6ef208a6298ad
vgurmeza <vgurmeza@xevo.com>
2019-10-11 10:56:58 -0700

    disabled enforcement of Pipfile, update to python3 for Ansible2.8

--- run_ansible.sh
+++ run_ansible.sh
@@ -53,7 +53,7 @@ inventory_checkout() {
   if [[ "${inventoryrepo+DEFINED}" ]]; then
 
     inventoryrepo_name=$(
-      echo "${inventoryrepo}" | 
+      echo "${inventoryrepo}" |
       perl -lane 'print $1 if /.*\/([\w-]+)\.git/'
     )
 
@@ -95,18 +95,20 @@ check_installed() {
   else
     echo "$1 is not installed.  Exiting..."
     exit 1
-  fi 
+  fi
 }
 
 pipenv_init() {
   check_installed pipenv
 
   # err out if either file exists but is NOT a symbolic link
-  if [[ -f "./Pipfile" && ! -h "./Pipfile" ]] || [[ -f "./Pipfile.lock" && ! -h "./Pipfile.lock" ]]; then
-    echo "Pipfile and Pipfile.lock should be a symbolic link, but is not."
-    echo "Backup and remove that file so this script can manage it."
-    exit 1
-  fi
+  # Pipfile and Pipfile.lock should be managed per project. If not is provided by
+  # we can reference the default Pipfile for ansible version
+  #if [[ -f "./Pipfile" && ! -h "./Pipfile" ]] || [[ -f "./Pipfile.lock" && ! -h "./Pipfile.lock" ]]; then
+  #  echo "Pipfile and Pipfile.lock should be a symbolic link, but is not."
+  #  echo "Backup and remove that file so this script can manage it."
+  #  exit 1
+  #fi
 
   # get real dir (in case symlink)
   local realdir=$(symlink_src_dir)
@@ -160,7 +162,7 @@ run_ansible_playbook() {
   echo "******** ------------"
   pipenv_init
   echo
- 
+
   echo "******** ----------------"
   echo "******** Init vault file "
   echo "******** ----------------"
