15b1ed87086d258bde55a12f32201c2fc0610e30
Shaun Smiley <ssmiley@xevo.com>
2020-02-28 13:41:06 -0800

    install libssl separately

--- run_ansible.sh
+++ run_ansible.sh
@@ -138,6 +138,12 @@ pipenv_init() {
           echo "--- update apt"
           echo "---------------------------------------------"
           sudo apt-get update
+
+          echo "---------------------------------------------"
+          echo "--- install libssl (pip dep)"
+          echo "---------------------------------------------"
+          sudo apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -qq -y install libssl1.1
+
           echo "---------------------------------------------"
           echo "--- install python3-pip"
           echo "---------------------------------------------"
